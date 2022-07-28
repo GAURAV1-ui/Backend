0e90a6f5a1a0a8a17dc00650e2bb02a8f37a42e1
Gaurav <iamgaurav201@gmail.com>
2022-07-28 23:45:49 +0530

    edit and delete updated

--- controllers/admin.js
+++ controllers/admin.js
@@ -4,9 +4,9 @@ exports.getAddProduct = (req, res, next) => {
   res.render('admin/add-product', {
     pageTitle: 'Add Product',
     path: '/admin/add-product',
-    // formsCSS: true,
-    // productCSS: true,
-    // activeAddProduct: true
+    formsCSS: true,
+    productCSS: true,
+    activeAddProduct: true
   });
 };
 
