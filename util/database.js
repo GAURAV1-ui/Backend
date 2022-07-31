8dab9ed274fb75885a8d6430aae636ce8293e927
Gaurav <iamgaurav201@gmail.com>
2022-07-31 21:44:47 +0530

    Mongodb updated

--- util/database.js
+++ util/database.js
@@ -1,17 +1,29 @@
 const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 
+let _db;
+
 const mongoConnect = callback => {
   MongoClient.connect(
-    'mongodb+srv://iamGaurav_201:56421556421@gGK@cluster0.7sq9r.mongodb.net/?retryWrites=true&w=majority'
+    'mongodb+srv://iamGaurav_201:56421556421@gGK@cluster0.7sq9r.mongodb.net/shop?retryWrites=true&w=majority'
   )
     .then(client => {
       console.log('Connected!');
-      callback(client);
+      _db = client.db();
+      callback();
     })
     .catch(err => {
       console.log(err);
+      throw err;
     });
 };
 
-module.exports = mongoConnect;
+const getDb = () => {
+  if (_db) {
+    return _db;
+  }
+  throw 'No database found!';
+};
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
