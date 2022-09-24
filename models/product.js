7f07adec1a37ef9ffc19992426fc2547e0923801
Gaurav <iamgaurav201@gmail.com>
2022-09-24 23:34:46 +0530

    updated

--- models/product.js
+++ models/product.js
@@ -2,18 +2,26 @@ const mongodb = require('mongodb');
 const getDb = require('../util/database').getDb;
 
 class Product {
-  constructor(title, price, description, imageUrl) {
+  constructor(title, price, description, imageUrl, id) {
     this.title = title;
     this.price = price;
     this.description = description;
     this.imageUrl = imageUrl;
+    this._id = id;
   }
- 
+
   save() {
     const db = getDb();
-    return db
-      .collection('products')
-      .insertOne(this)
+    let dbOp;
+    if (this._id) {
+      // Update the product
+      dbOp = db
+        .collection('products')
+        .updateOne({ _id: new mongodb.ObjectId(this._id) }, { $set: this });
+    } else {
+      dbOp = db.collection('products').insertOne(this);
+    }
+    return dbOp
       .then(result => {
         console.log(result);
       })
@@ -36,19 +44,20 @@ class Product {
         console.log(err);
       });
   }
+
   static findById(prodId) {
     const db = getDb();
     return db
-    .collections('poducts')
-    .find({_id: new mongodb.ObjectId(prodId)})
-    .next()
-    .then(product => {
-      console.log(product);
-      return product;
-    })
-    .catch(err => {
-      console.log(err);
-    })
+      .collection('products')
+      .find({ _id: new mongodb.ObjectId(prodId) })
+      .next()
+      .then(product => {
+        console.log(product);
+        return product;
+      })
+      .catch(err => {
+        console.log(err);
+      });
   }
 }
 
