068b6db1fe43640b877d2c91e1d2e0d1e1132633
Gaurav <iamgaurav201@gmail.com>
2022-11-03 02:57:37 +0530

    New added

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const errorController = require('./controllers/error');
-const mongoConnect = require('./util/database').mongoConnect;
 const User = require('./models/user');
 
 const app = express();
@@ -32,6 +32,10 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect(() => {
+mongoose
+.connect('mongodb+srv://iamGaurav_201:542155421@cluster0.7sq9r.mongodb.net/?retryWrites=true&w=majority').then(result => {
   app.listen(3000);
+})
+.catch(err =>{
+  console.log(err);
 });
