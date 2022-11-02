0a42702116826d7a62548f7d7923136f7e78fbe8
Gaurav <iamgaurav201@gmail.com>
2022-11-03 02:34:16 +0530

    last commit

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-  User.findById('633d2f32efa0215a91124916')
+  User.findById('5baa2528563f16379fc8a610')
     .then(user => {
       req.user = new User(user.name, user.email, user.cart, user._id);
       next();
