ba282616e9ae19bffde2a1453a041e84459a4159
GAURAV <iamgaurav201@gmail.com>
2023-05-05 02:43:59 +0530

    user cart added

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-  User.findById('5bab316ce0a7c75f783cb8a8')
+  User.findById('64541e08f16f443f04a124b8')
     .then(user => {
       req.user = user;
       next();
